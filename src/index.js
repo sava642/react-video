ab7b852eaf4a91c8a4eae22bf87cfb5697413949
sava642 <101993338+sava642@users.noreply.github.com>
2022-11-06 12:43:36 +0100

    main

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
