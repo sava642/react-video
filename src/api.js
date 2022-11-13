ed256e8de852a42f92b07b22bb09752b35c82857
sava642 <101993338+sava642@users.noreply.github.com>
2022-11-13 08:45:46 +0100

    main

--- src/api.js
+++ src/api.js
@@ -3,10 +3,6 @@ import axios from 'axios';
 const REACT_APP_API_KEY = `6156c956c00552a373a30b2f4ae90c99`;
 const BASE_URL = axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-
-
-
-
 export const fetchTrendingToday = async (url) => {
 	const response = await axios.get(`${BASE_URL}${url}`, {
 		params: {
--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -1,8 +1,6 @@
 import { HiArrowLeft } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
-
-
 export const BackLink = ({ to, children }) => {
   return (
     <Link to={to} style={{
